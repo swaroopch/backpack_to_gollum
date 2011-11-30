df5b3e99b70daec061c3d6b731a0e105ed6d9f6a
Swaroop C H <swaroop@swaroopch.com>
2011-11-30 23:20:04 +0530

    Handling Asset, Gallery, Email, WriteboardLink

--- backpack_to_gollum.rb
+++ backpack_to_gollum.rb
@@ -7,6 +7,7 @@ require 'erb'
 require 'iconv'
 
 require 'rubygems' unless defined? Gem
+require 'bundler/setup'
 require 'xmlsimple'
 require 'pandoc-ruby'
 
@@ -30,8 +31,17 @@ input = XmlSimple.xml_in(File.read('export.xml'))
 
 pages = {}
 
+def pluralize(text)
+  if text =~ /y$/
+    text = text.sub(/y$/, 'ies')
+  else
+    text += "s"
+  end
+  text
+end
+
 def get_by_id(page, type, id)
-  outputs = page["#{type}s"][0][type].select { |data| data['id'] == id }
+  outputs = page[pluralize(type)][0][type].select { |data| data['id'] == id }
   if outputs.size > 0
     outputs[0]
   else
@@ -78,6 +88,34 @@ def fetch_content(belonging, page)
 * [] <%= item['content'] %><% end %><% end %><% end %>
 }
     ERB.new(template).result(binding)
+  when 'Asset'
+    data = get_by_id(page, 'attachment', widget_id)
+    template = %Q{
+TBD file <%= data['file_name'] %>
+}
+    ERB.new(template).result(binding)
+  when 'Email'
+    data = get_by_id(page, 'email', widget_id)
+    template = %Q{
+TBD email <%= data['subject'] %>
+}
+    ERB.new(template).result(binding)
+  when 'Gallery'
+    data = get_by_id(page, 'gallery', widget_id)
+    template = %Q{
+# <%= data['name'] %>
+<% data['images'][0]['image'].each do |image| %>
+TBD <%= image['description'] %> ( <%= image['file_name'] %> )
+<% end %>
+}
+    ERB.new(template).result(binding)
+  when 'WriteboardLink'
+    template = %Q{
+TBD <%= widget_type %> <%= widget_id %> (There is no identifying information about the name of the Writeboard)
+}
+    ERB.new(template).result(binding)
+  else
+    @log.error("Haven't handled #{widget_type}")
   end
 end
 
